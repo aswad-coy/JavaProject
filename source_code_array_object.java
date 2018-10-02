b0ccf65dea1941962536fd6360bdbad65d209db3
aswad-coy <43776228+aswad-coy@users.noreply.github.com>
2018-10-02 15:35:28 +0700

    Add files via upload

---
+++ source_code_array_object.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class source_code_array_object {
+
+	
+	public static void main(String[] args) {
+		// DEKLARASI VARIABEL
+		Object[] animals= new Object[2];
+		//INISIALISASI VALUE
+		animals[0] = "cat";
+		animals[1] = "rabbit";
+		//MENAMPILKAN HASIL
+		System.out.println("jenis peliharaan saya : " + animals[0]);
+		System.out.println("jenis peliharaan saya : " + animals[1]);
+		//MENGAMBIL IMPUT DARI KEYBOARD
+		Object[] animals1= new Object[2];
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("masukkan nama kucing :");
+		animals1[0] = keyboard.next();
+		System.out.println("masukan nama kelinci :");
+		animals1[1] = keyboard.next();
+		System.out.println("nama kucing saya : " + animals1[0]);
+		System.out.println("nama kucing saya : " + animals1[1]);
+
+
+	}
+
+}
---
+++ source_code_array_string.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class source_code_array_string {
+
+	
+	public static void main(String[] args) {
+		// DEKLARASI VARIABEL
+		String[] mahasiswa = new String[6];
+		//INISIALISASI VALUE
+		mahasiswa[0] = "muhammad mohar";
+		mahasiswa[1] = "aswad";
+		mahasiswa[2] = "aswandi";
+		mahasiswa[3] = "udin";
+		mahasiswa[4] = "sadly";
+		mahasiswa[5] = "rosalina konde";
+		//OPERASI VARIABEL
+		System.out.println("DAFTAR NIM____");
+		String[] nim = {"D0217321","D0217319","D0217011","D0217017","D0217328","D0217012"};
+		for (String element: nim) {System.out.println(element);}
+		//MENAMPILKAN HASIL
+		System.out.println("PEMILIK NIM______");
+		for(int i=0;i<mahasiswa.length;i++){System.out.println((mahasiswa[i]));};
+		//MENGAMBIL IMPUT DARI KEYBOARD
+	    String[] mahasiswa_tambahan = new String[5];
+	    Scanner keyboard = new Scanner(System.in);
+	for( int i = 0; i < mahasiswa_tambahan.length; i++ )
+	    {System.out.print(i);
+	    mahasiswa_tambahan[i] = keyboard.nextLine();}
+	    System.out.println("--mahasiswa tambahan--");
+	for(int i=0;i<mahasiswa_tambahan.length;i++){System.out.println(mahasiswa_tambahan[i]);};
+
+
+	}
+
+}
